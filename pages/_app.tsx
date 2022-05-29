e26ff40d20b3be7999815fc84f6f66ed518376b8
yuikoito <toffywing@gmail.com>
2022-05-29 17:06:31 +0900

    chore: update font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import '@fontsource/m-plus-rounded-1c';
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
+import { theme } from '../thema';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   );
