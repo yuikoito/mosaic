be0cef3ed7e36493dd1c622b9fcd2c83e8f0bb0d
yuikoito <toffywing@gmail.com>
2022-05-29 15:50:03 +0900

    chore: import chakraUI && update next.config.js for i18n

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  i18n: {
+    locales: ['ja', 'en'],
+    defaultLocale: 'ja',
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+      };
+    }
 
-module.exports = nextConfig
+    return config;
+  },
+};
+
+module.exports = nextConfig;
